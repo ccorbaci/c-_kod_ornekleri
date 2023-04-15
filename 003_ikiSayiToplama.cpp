ab0333f5f1a7be4d62979da88e101544551e2b58
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2023-04-16 02:31:39 +0300

    Create 003_ikiSayiToplama.cpp

---
+++ 003_ikiSayiToplama.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sayi1 = 1;
+    int sayi2 = 2;
+    int toplam;
+
+        toplam = sayi1 + sayi2;
+
+
+        cout << "Girdiginiz sayilarin toplami: " << endl;
+        cout << toplam << endl;
+    
+
+    return 0;
+}
