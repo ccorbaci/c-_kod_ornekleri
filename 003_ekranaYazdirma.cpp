49fe0c7ce927e57ed69d3c775b61f0ce89c1a800
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2023-04-16 03:18:30 +0300

    Create 003_ekranaYazdirma.cpp

---
+++ 003_ekranaYazdirma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string Name = "Ahmet";
+    int Age = 35;
+    
+    cout << "Benim adim: " << Name << endl;
+    cout << "Yasim " << Age << endl;
+
+
+    return 0;
+}
