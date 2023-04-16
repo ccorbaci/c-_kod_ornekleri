7d34657505908abeba7841358b13333d78f5bf21
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2023-04-16 05:25:10 +0300

    Create 008_kullanıcıdanGirisAlma.cpp

---
+++ 008_kullanıcıdanGirisAlma.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Yasinizi Giriniz:" << endl;
+    cin >> age;
+    cout << "Siz " << age <<" yasindasiniz"<< endl;
+
+    return 0;
+}
