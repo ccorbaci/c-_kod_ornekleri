d2a39ed2ed1439c04912a635c700edb75a6f6521
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2023-04-16 04:57:50 +0300

    Update 006_sayilar.cpp

--- 006_sayilar.cpp
+++ 006_sayilar.cpp
@@ -8,12 +8,15 @@ int main()
 
     //tamsayi
     int number = 5;
+    cout << number << endl;
 
     //Ondalıklı Tamsayı.Bellekte 4 byte yer tutar.ondalık sayı hassasiyeti 6 haneli bir kesirli sayıya kadar doğru olabilir.
-    float nUmber2 = 5.5;
+    float number2 = 5.5;
+    cout << number2 << endl;
 
     //Ondalıklı Tamsayı.Bellekte 8 byte bellek kullanır ve ondalık sayı hassasiyeti 15 haneli bir kesirli sayıya kadar doğru olabilir.
-    double number3 = 5.5;
+    double number3 = 6.5;
+    cout << number3 << endl;
 
     //Temel aritmetik ifadeler " + - * / "  
     cout << 30 + 40 << endl;
@@ -25,8 +28,13 @@ int main()
     //Parantezli işleri bu sekilde yapabilirsiniz.Burada 90 verecek.
     cout << (4 + 5) * 10 << endl;
 
+    //sayının bir fazlası istiyorsak ++ kullanırız.Döngüler gibi durumlarda bu kısaltma gerekli olacak.-- de 1 azaltır.
+    //Örneğin +=5 sayıya 5 ekleyecektir.
+    number++;
+    cout << number << endl;
 
-
+    //2 tamsayıyı böldüğümüzde sonucu tamsayı alırız.Sonucu ondalıklı istiyorsak sayıları da ondalıklı vermemiz gerek.
+    cout << 10.0 / 3.0 << endl;     
 
     return 0;
 }
