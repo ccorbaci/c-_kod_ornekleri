5b865a612245c6853ab55ae386d839e92d74a292
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2023-04-16 22:33:23 +0300

    Create 014_ifKosulu1.cpp

---
+++ 014_ifKosulu1.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	bool isMale = false;
+
+	
+	if (isMale) {
+
+		cout << "You are a male";
+
+	}
+	else{
+
+		cout << "You are a famale";
+	}
+
+
+	return 0;
+
+}
+ 
