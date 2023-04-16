02a18ccac0f881a13fc2eda6d289e580d470325e
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2023-04-16 03:38:55 +0300

    Create 004_veriTipleri.cpp

---
+++ 004_veriTipleri.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Tek Karakter dizesi
+    char grade = 'A';
+    //Karakter katarı
+    string phrase = "Merhaba Hosgeldiniz";
+    //Tamsayi
+    int number = 100;
+    //ondalıklı sayı
+    float number2 = 100.95;
+    //True-False
+    bool isMale = true;
+
+    return 0;
+}
