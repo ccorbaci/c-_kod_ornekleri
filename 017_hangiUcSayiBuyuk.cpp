426b606b130a75cf206508b17342f474c7c77dfe
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2023-04-17 21:03:07 +0300

    Update 017_hangiUcSayiBuyuk.cpp

--- 017_hangiUcSayiBuyuk.cpp
+++ 017_hangiUcSayiBuyuk.cpp
@@ -5,11 +5,9 @@ int getMax(int num1, int num2,int num3) {
     int result;
     if (num1 >= num2 && num1 >= num3) {
         result = num1;
-    }
-    else if (num2 >= num1 && num2 >= num3) {
+    }else if (num2 >= num1 && num2 >= num3) {
         result = num2;
-    }
-    else {
+    }else {
         result = num3;
     }
 
