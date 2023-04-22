d518b8fd4d31685a529f6214424c263d35392e6a
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2023-04-22 20:10:33 +0300

    Create 036_veriBoyutlari.cpp

---
+++ 036_veriBoyutlari.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+    cout << " Veri Boyutlari" << endl;
+    cout << "char \t\t\t: " << sizeof(char) << endl;
+    cout << "int \t\t\t: " << sizeof(int) << endl;
+    cout << "short int (int 16)) \t: " << sizeof(short int) << endl;
+    cout << "long int (int 32)\t: " << sizeof(long) << endl;
+    cout << "long int (int 32)\t: " << sizeof(long int) << endl;
+    cout << "long long int (int 64) \t: " << sizeof(long long int) << endl;
+    cout << "float \t\t\t: " << sizeof(float) << endl;
+    cout << "double \t\t\t: " << sizeof(double) << endl;
+    return 0;
+}
