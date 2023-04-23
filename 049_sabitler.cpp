fe1ba6bceb6e267440a2c889f515f60e2aa14d14
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2023-04-23 23:27:08 +0300

    Create 049_sabitler.cpp

---
+++ 049_sabitler.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+Sabitler
+
+Değişmez ya da sabit,değeri değişmeyen program bileşenidir.
+Değişmez olaran yer alabilen veri türleri
+	int
+	float
+	char
+	string
+Bir değişmez tanımlandıktan sonra programın başka bir yerinde değerini değiştirmek olanaksızdır.
+
+
+*/
+
+
+float const PI = 3.14;
+char const cinsiyet = 'E';
+
+int main() {
+
+	cout << "PI = " <<PI<< endl;
+	cout << "Cinsiyet = " << cinsiyet << endl;
+
+
+	system("PAUSE");
+
+	return 0;
+}
+
